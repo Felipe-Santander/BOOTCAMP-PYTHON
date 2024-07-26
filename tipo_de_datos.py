280f584f0a7771b4c4b3aa2f8432e3a756d3bcc7
Felipe-Santander <felipevillota2002@gmail.com>
2024-07-26 13:23:27 -0500

    se inicializa el repositorio de python

---
+++ tipo_de_datos.py
@@ -0,0 +1,16 @@
+#tipo de datos 
+#string texto
+"felipe"
+
+nombre = "felipe"
+
+#tipo de dato numero
+
+#    1 2 3 4 5 6 7 8 9   tipo de datos enteros (int)
+#     1.0  2.04  3.22  tipo de datos flotantes (float)
+
+
+#tipo de datos booleanos
+
+True # Verdadero T va con mayusculas
+False # Falso la primera F va mayuscula
